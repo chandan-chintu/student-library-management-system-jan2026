a41c3c3713b1d47a68efa6bedd7928688277633b
Chandan K M <chandanchintu16@gmail.com>
2026-01-31 16:53:50 +0530

    Find + update + delete APIs and pagination + sorting APIs and exception handling in APIs and custom queries

--- src/main/java/com/example/student_library_management_system/controller/StudentController.java
+++ src/main/java/com/example/student_library_management_system/controller/StudentController.java
@@ -1,12 +1,12 @@
 package com.example.student_library_management_system.controller;
 
+import com.example.student_library_management_system.model.Student;
 import com.example.student_library_management_system.requestdto.StudentRequestDto;
 import com.example.student_library_management_system.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student/apis")
@@ -20,4 +20,51 @@ public class StudentController {
         String response = studentService.saveStudent(studentRequestDto);
         return response;
     }
+
+    @GetMapping("/findById/{id}")
+    public Object findStudentById(@PathVariable int id){
+        try {
+            Student student = studentService.findStudentById(id);
+            return student;
+        } catch (Exception e){
+            return "Exception occurred : "+e.getMessage()+"---"+e.getClass();
+        }
+    }
+
+    @GetMapping("/findAll")
+    public List<Student> findAllStudents(){
+       List<Student> studentList = studentService.findAllStudents();
+       return studentList;
+    }
+
+    @GetMapping("/findAllByPage")
+    public List<Student> findAllStudentsByPage(@RequestParam int pageNo,@RequestParam int pageSize){
+        List<Student> studentList = studentService.getStudentByPage(pageNo, pageSize);
+        return studentList;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudentById(@PathVariable int id){
+        String response = studentService.deleteStudentById(id);
+        return response;
+    }
+
+    @PutMapping("/update/{id}")
+    public String updatedStudent(@PathVariable int id,@RequestBody StudentRequestDto studentRequestDto){
+        String response = studentService.updateStudentUsingPut(id, studentRequestDto);
+        return response;
+    }
+
+    @GetMapping("/findByEmail")
+    public Student findStudentByEmail(@RequestParam String email){
+        Student student = studentService.getStudentByEmail(email);
+        return student;
+    }
+
+    @GetMapping("/findByDept")
+    public List<Student> findStudentByDept(@RequestParam String dept){
+        List<Student> studentList = studentService.getStudentByDept(dept);
+        return studentList;
+    }
+
 }
--- src/main/java/com/example/student_library_management_system/service/StudentService.java
+++ src/main/java/com/example/student_library_management_system/service/StudentService.java
@@ -6,9 +6,13 @@ import com.example.student_library_management_system.model.Student;
 import com.example.student_library_management_system.repository.StudentRepository;
 import com.example.student_library_management_system.requestdto.StudentRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -38,5 +42,87 @@ public class StudentService {
         studentRepository.save(student);
         return "Student saved successfully!";
 
+    }
+
+    public List<Student> findAllStudents(){
+        List<Student> studentList=studentRepository.findAll();
+        return studentList;
+    }
+
+     /*
+    Pagination - fetching or getting the records or data in the form of pages
+    pagenumber - the number of page we want to see(0,1,2,3,4,5...)
+    pagesize - total number of records in each page(fixed for each page)
+
+    total number of record - 28, page size - 5
+    0th page - 1-5
+    1st page - 6-10
+    2nd page - 11-15
+    3rd page - 16-20
+    4th page - 21-25
+    5th page - 26-28
+
+    total numbers of records-11, page size-3
+    0th page - 1-3
+    1st page - 4-6
+    2nd page - 7-9
+    3rd page - 10-11
+
+    // only pagination
+    public List<Student> getStudentByPage(int pageNo, int pageSize){
+        List<Student> studentList= studentRepository.findAll(PageRequest.of(pageNo,pageSize)).getContent();
+        return studentList;
+    }
+
+      */
+
+    // pagination along with sorting
+    public List<Student> getStudentByPage(int pageNo, int pageSize){
+        List<Student> studentList= studentRepository.findAll(PageRequest.of(pageNo,pageSize, Sort.by("name"))).getContent();
+        return studentList;
+    }
+
+    public Student findStudentById(int id){
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        if(studentOptional.isPresent()){
+            return studentOptional.get();
+        } else {
+            throw new RuntimeException("Student not found with id : "+id);
+        }
+    }
+
+    public String deleteStudentById(int id){
+        studentRepository.deleteById(id);
+        return "Student with id : "+id+" deleted successfully!";
+    }
+
+    public String updateStudentUsingPut(int studentId, StudentRequestDto newStudentRequestDto){
+        Student existingStudent = findStudentById(studentId);
+        if (existingStudent!=null){
+            existingStudent.setName(newStudentRequestDto.getName());
+            existingStudent.setEmail(newStudentRequestDto.getEmail());
+            existingStudent.setMobile(newStudentRequestDto.getMobile());
+            existingStudent.setDob(newStudentRequestDto.getDob());
+            existingStudent.setAddress(newStudentRequestDto.getAddress());
+            existingStudent.setSem(newStudentRequestDto.getSem());
+            existingStudent.setGender(newStudentRequestDto.getGender());
+            existingStudent.setDept(newStudentRequestDto.getDept());
+
+            studentRepository.save(existingStudent);
+            return "Student updated successfully!";
+        }else{
+            return "Student with id : "+studentId+" not found! hence cannot update";
+        }
+
+    }
+
+    public Student getStudentByEmail(String email){
+        Student student = studentRepository.getStudentByEmail(email);
+        return  student;
+    }
+
+    public List<Student> getStudentByDept(String dept){
+        List<Student> studentList = studentRepository.getStudentByDept(dept);
+        return studentList;
     }
 }
